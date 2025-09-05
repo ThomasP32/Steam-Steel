04804b6eb63864a6488d0e25f36d2d2c2ebf9981
ThomasP32 <thomas.perron-duveau@polymtl.ca>
2025-09-04 22:00:24 -0400

    fix(CI/CD): fix script for mobile 6

--- mobile/lib/router/app_router.dart
+++ mobile/lib/router/app_router.dart
@@ -2,7 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:mobile/main.dart';
 import 'package:mobile/screens/joingame_screen.dart';
 
-/// Defines the application router using GoRouter
+// Defines the application router using GoRouter
 
 class AppRouter {
   static final GoRouter router = GoRouter(
