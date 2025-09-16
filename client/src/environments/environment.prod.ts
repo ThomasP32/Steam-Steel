0201e86c670c65b6e83319f3c316ef9b9be687f5
Noemie <noemie.helias@polymtl.ca>
2025-09-16 09:46:45 -0400

    chore(deploy): change server URL in environments

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://localhost:3000/api',
-    //serverUrl: 'http://ec2-3-96-63-155.ca-central-1.compute.amazonaws.com:3000/api',
-    socketUrl: 'http://ec2-3-96-63-155.ca-central-1.compute.amazonaws.com:3000/game',
+   // serverUrl: 'http://localhost:3000/api',
+    serverUrl: 'http://ec2-35-183-61-112.ca-central-1.compute.amazonaws.com:3000/api',
+    socketUrl: 'http://ec2-35-183-61-112.ca-central-1.compute.amazonaws.com:3000/game',
 };
